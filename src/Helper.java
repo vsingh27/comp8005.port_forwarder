8b5339a970b3fe167fff72a4c0e0dd9d79fd1988
Rizwan <rizwan_ahmed21@hotmail.com>
2016-03-15 15:40:26 -0700

    created helper class

---
+++ src/Helper.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Rizwan Ahmed on 15/03/2016.
+ */
+import java.util.*;
+public class Helper {
+
+    public Map createMap()
+    {
+        Map<Object, Object> map = new HashMap<Object,Object>();
+        return map;
+    }
+
+    public void addObjects(HashMap map,Object key, Object value)
+    {
+        map.put(key, value);
+
+    }
+}
