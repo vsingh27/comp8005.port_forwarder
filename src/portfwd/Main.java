ac866236ea7596499e080b5d6eafd199cd04612d
Rizwan Ahmed <rizwan_ahmed21@hotmail.com>
2016-03-16 00:03:47 -0700

    got rid of sample mentions
    there were a bunch of "sample.Main" everywhere causing compile to fail

--- src/portfwd/Main.java
+++ src/portfwd/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("portfwd.fxml"));
+        primaryStage.setTitle("Port Forwarder");
+        primaryStage.setScene(new Scene(root, 730.0D, 492.0D));
         primaryStage.show();
     }
 
