90584495f99a6d5715059496fe17d6ec44599978
Rizwan Ahmed <rizwan_ahmed21@hotmail.com>
2016-04-03 15:48:51 -0700

    added the socket functionality
    only listening on port 0 needs to be fixed

--- src/portfwd/Controller.java
+++ src/portfwd/Controller.java
@@ -10,8 +10,16 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Pair;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 //This is the intial commit
 public class Controller {
@@ -38,6 +46,9 @@ public class Controller {
     private ForwardPair tempFPO;
 
     private boolean isTableMade = false;
+    private int sourcePort = 0;
+    private int destinationPort = 0;
+    private String destinationIP = null;
 
 
 
@@ -148,7 +159,82 @@ public class Controller {
     public void StartButtonClicked()
     {
         appConsole.appendText("\n Start button clicked!");
+        run();
 
     }
 
+    public void run() {
+        try {
+            ConnectionTracker tracker = new ConnectionTracker();
+            Helper helper = new Helper();
+
+            //Create A ServerSocketChannel To Listen on the given port
+            ServerSocketChannel servSockChannel = helper.makeNonBlockingServerSocketChannnel();
+            //Bind ther serverSocketChannel to the port
+            helper.bindServerSocketChannel(servSockChannel, sourcePort);
+
+            //create a selector
+            Selector selector = helper.createSelector();
+            //register ServerSocketChannel with this selector with OP_ACCPET
+            boolean registerSelectorOK = helper.registerSelector(servSockChannel, selector, 3);
+            System.out.println("Listening on PORT " + sourcePort);
+            ConnectionTracker connectionTracker = new ConnectionTracker();
+
+            while (true) {
+                int num = helper.select(selector);
+
+                //IF no activity loop back
+                if (num == 0) {
+                    continue;
+                }
+
+                //Get the Keys of the corresponding activity and process one by one
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> it = keys.iterator();
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+                    //If There are new Connections
+                    if ((key.readyOps() & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT) {
+                        System.out.println("Accepting New Connection...");
+
+                        SocketChannel sourceSocketChannel = servSockChannel.accept();
+                        //Set this socketChannel to Non Blocking
+                        helper.setSocketChannelNonBlocking(sourceSocketChannel);
+                        //Register this socket For OP_READ
+                        helper.registerSocketChannel(sourceSocketChannel, selector, 1);
+                        //Make the forward Socket
+                        SocketChannel newSocChannel = helper.makeForwardingSocket(sourceSocketChannel, this.destinationPort, this.destinationIP, selector);
+                        //Add the Forward Socket and the client socket to the MAP
+                        connectionTracker.populateSocketTracker(sourceSocketChannel, newSocChannel);
+                        System.out.println("Connection Established with Server...");
+                    } else if ((key.readyOps() & SelectionKey.OP_READ) == SelectionKey.OP_READ) {
+                        SocketChannel receiveChannel = null;
+
+                        try {
+                            //Process the incoming data
+                            receiveChannel = (SocketChannel) key.channel();
+                            SocketChannel forwardChannel = tracker.getForwardingSocketFromSocketTracker(receiveChannel);
+                            boolean processDataOK = helper.processData(receiveChannel,forwardChannel);
+                            if(!processDataOK)
+                            {
+                                key.cancel();
+                                try{
+                                    receiveChannel.close();
+                                }catch (IOException io)
+                                {
+                                    System.out.println("Error closing socket...");
+                                    io.printStackTrace();
+                                }
+                            }
+                        } catch (Exception io) {
+
+                        }
+                    }
+                }
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
+    }
 }
--- src/portfwd/Helper.java
+++ src/portfwd/Helper.java
@@ -14,6 +14,7 @@ import java.util.TreeMap;
 
 public class Helper
 {
+    private final int BUFFERSIZE = 2048;
 
     /**
      * @param map
@@ -41,8 +42,7 @@ public class Helper
      * @return A ServerSocketChannel configured with Non Blocking option
      * @throws IOException
      */
-    protected ServerSocketChannel makeNonBlockingServerSocketChannnel() throws IOException, SocketException
-    {
+    protected ServerSocketChannel makeNonBlockingServerSocketChannnel() throws IOException, SocketException {
 
         //Opening up a server Socket Channel
         ServerSocketChannel ssChannel = ServerSocketChannel.open();
@@ -50,8 +50,8 @@ public class Helper
         ssChannel.configureBlocking(false);
         //Set the options of this channel
         //In this case we will set it to SO_LINGER and SO_REUSEADDR
-        ssChannel.setOption(StandardSocketOptions.SO_LINGER, null);
-        ssChannel.setOption(StandardSocketOptions.SO_REUSEADDR, null);
+        //ssChannel.setOption(StandardSocketOptions.SO_LINGER, null);
+        ssChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
 
         return ssChannel;
     }
@@ -63,15 +63,12 @@ public class Helper
      * @param port The Listening port to Bind the ServerSocket
      * @return True if binding was successful, False if binding was unsuccessful
      */
-    protected boolean bindServerSocketChannel(ServerSocketChannel ssc, int port)
-    {
-        try
-        {
+    protected boolean bindServerSocketChannel(ServerSocketChannel ssc, int port) {
+        try {
             ServerSocket srvsock = ssc.socket();
             InetSocketAddress isa = new InetSocketAddress(port);
             srvsock.bind(isa);
-        } catch (IOException io)
-        {
+        } catch (IOException io) {
             System.out.println(io.getMessage());
             return false;
         }
@@ -84,8 +81,7 @@ public class Helper
      * @return Selector
      * @throws IOException
      */
-    protected Selector createSelector() throws IOException
-    {
+    protected Selector createSelector() throws IOException {
         Selector selector = Selector.open();
         return selector;
     }
@@ -105,10 +101,8 @@ public class Helper
      * @throws ClosedChannelException
      */
     protected boolean registerSelector(ServerSocketChannel ssc, Selector selector, int option) throws ClosedSelectorException
-        , IllegalBlockingModeException, CancelledKeyException, IllegalArgumentException, ClosedChannelException
-    {
-        switch (option)
-        {
+            , IllegalBlockingModeException, CancelledKeyException, IllegalArgumentException, ClosedChannelException {
+        switch (option) {
             case 1:
                 ssc.register(selector, SelectionKey.OP_READ);
                 System.out.println();
@@ -131,33 +125,74 @@ public class Helper
     }
 
     /**
+     * Registers a selector for the ACCEPT, READ, or WRITE
+     *
+     * @param sc
+     * @param selector
+     * @param option   Operation Set Bit (1=OP_READ, 2=OP_WRITE, 3=OP_CONNECT)
+     * @return True on Success and False on Failure
+     * @throws ClosedSelectorException
+     * @throws IllegalBlockingModeException
+     * @throws CancelledKeyException
+     * @throws IllegalArgumentException
+     * @throws ClosedChannelException
+     */
+    protected boolean registerSocketChannel(SocketChannel sc, Selector selector, int option) throws ClosedSelectorException
+            , IllegalBlockingModeException, CancelledKeyException, IllegalArgumentException, ClosedChannelException {
+        switch (option) {
+            case 1:
+                sc.register(selector, SelectionKey.OP_READ);
+                System.out.println();
+                return true;
+            case 2:
+                sc.register(selector, SelectionKey.OP_WRITE);
+                System.out.println();
+                return true;
+            case 3:
+                sc.register(selector, SelectionKey.OP_CONNECT);
+                System.out.println();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    /**
      * Creates a Socket Channel that is set in the non-blocking mode
      *
      * @return A Socket Channel
      * @throws IOException
      */
-    protected SocketChannel makeNonBlockingSocketChannnel() throws IOException
-    {
+    protected SocketChannel makeNonBlockingSocketChannnel() throws IOException {
         SocketChannel sockChannel = SocketChannel.open();
         sockChannel.configureBlocking(false);
-        sockChannel.setOption(StandardSocketOptions.SO_LINGER, null);//Set the option to SO_LINGER
-        sockChannel.setOption(StandardSocketOptions.SO_REUSEADDR, null);//Set the option to SO_REUSEADDR
+        //sockChannel.setOption(StandardSocketOptions.SO_LINGER, true);//Set the option to SO_LINGER
+        sockChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);//Set the option to SO_REUSEADDR
         return sockChannel;
+    }
+
+    protected void setSocketChannelNonBlocking(SocketChannel sc) {
+        try {
+            sc.configureBlocking(false);
+            // sc.setOption(StandardSocketOptions.SO_LINGER, null);//Set the option to SO_LINGER
+            sc.setOption(StandardSocketOptions.SO_REUSEADDR, true);//Set the option to SO_REUSEADDR
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
     }
 
     /**
      * Connects the Socket Channel to the provided address
+     *
      * @param sChannel The Socket Channel
-     * @param addr The Inet Address
+     * @param addr     The Inet Address
      * @return True on success and False on Failure
      */
-    protected boolean connectSocketChannel(SocketChannel sChannel, InetSocketAddress addr)
-    {
-        try
-        {
+    protected boolean connectSocketChannel(SocketChannel sChannel, InetSocketAddress addr) {
+        try {
             sChannel.connect(addr);
-        } catch (IOException io)
-        {
+        } catch (IOException io) {
             io.printStackTrace();
             return false;
         }
@@ -165,17 +200,15 @@ public class Helper
     }
 
     /**
-     *Closes the Socket Channel
+     * Closes the Socket Channel
+     *
      * @param sChannel The Socket Channel to close
      * @return True on success and False on failure
      */
-    protected boolean closeSocketChannel(SocketChannel sChannel)
-    {
-        try
-        {
+    protected boolean closeSocketChannel(SocketChannel sChannel) {
+        try {
             sChannel.close();
-        }catch(IOException io)
-        {
+        } catch (IOException io) {
             io.printStackTrace();
             return false;
         }
@@ -183,22 +216,73 @@ public class Helper
     }
 
     /**
-     *Reads the Socket Channel into a ByteBuffer
+     * Reads the Socket Channel into a ByteBuffer
+     *
      * @param sChannel Socket Channel To read from
      * @return ByteBuffer
      */
-    protected ByteBuffer readSocketChannel(SocketChannel sChannel)
-    {
+    protected ByteBuffer readSocketChannel(SocketChannel sChannel) {
         ByteBuffer buf = ByteBuffer.allocate(1048);
-        try
-        {
+        try {
             int bytesRead = sChannel.read(buf);
-        }catch (IOException io)
-        {
+        } catch (IOException io) {
             io.printStackTrace();
         }
 
         return buf;
+    }
+
+    protected int select(Selector selector) {
+        int num = 0;
+        try {
+            num = selector.select();
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+        return num;
+    }
+
+    protected SocketChannel makeForwardingSocket(SocketChannel sc, int port, String hostIP, Selector selector) {
+        InetSocketAddress address = new InetSocketAddress(hostIP, port);
+        SocketChannel socChannel = null;
+        try {
+            socChannel = SocketChannel.open();
+            //Connect to the forwarding host
+            socChannel.connect(address);
+            //register the socket for OP_READ
+            registerSocketChannel(socChannel, selector, 1);
+
+        } catch (IOException io) {
+            io.printStackTrace();
+            try {
+                sc.close();
+                socChannel.close();
+            } catch (IOException io2) {
+                io2.printStackTrace();
+            }
+        }
+        return socChannel;
+    }
+
+    protected boolean processData(SocketChannel recieveChannel, SocketChannel forwardSocketChannel) {
+        ByteBuffer buffer = ByteBuffer.allocate(BUFFERSIZE);
+        int bytesRead;
+        try {
+            recieveChannel.read(buffer);
+            buffer.flip();
+
+            //if no data close socket
+            if(buffer.limit() == 0)
+            {
+                return false;
+            }
+
+            forwardSocketChannel.write(buffer);
+            System.out.println("Processed Data and sent to the corresponding socket....");
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+        return true;
     }
 
 }
